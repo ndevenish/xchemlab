7c54aace165738a27e086e6ae1ac505f6cbc182e
Nicholas Devenish <ndevenish@gmail.com>
2023-05-25 08:59:34 +0100

    Tidy test gathering script
    - Shebang
    - No error if folder exists
    - Print copying output

--- collect_test_data.sh
+++ collect_test_data.sh
@@ -1,6 +1,10 @@
-mkdir test_data
+#!/usr/bin/env bash
+mkdir -p test_data
 for FILE_NAME in $(find /dls/labxchem/data/ -mindepth 5 -maxdepth 5 -path */processing/database/soakDBDataFile.sqlite ! -path */20*)
 do
     VISIT=$(cut -d / -f 6 <<< $FILE_NAME)
-    cp "$FILE_NAME" "test_data/$VISIT.sqlite"
+    (
+        set -x
+        cp "$FILE_NAME" "test_data/$VISIT.sqlite"
+    )
 done
